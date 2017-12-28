31975e62d0813e02a0e66e23cb664b89c08fcc28
Oleg Dudkin <dudkin.on@gmail.com>
2017-12-27 18:46:35 -0600

    docs: Add the comments

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 (function () {
-
+  // define variables
   var elem = document.getElementById('cat-container');
   var count = document.getElementById('counter');
   var counter = 0;
+
+  // create event
   elem.addEventListener('click', function() {
+    /**
+     * Increment counter, and display result
+     * @type {number}
+     */
     counter += 1;
     count.innerHTML = counter;
   }, false);
