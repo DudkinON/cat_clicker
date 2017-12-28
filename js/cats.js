b2bd4c348e4ba89bd8df3a97a2eb16f2a7feed3a
Oleg Dudkin <dudkin.on@gmail.com>
2017-12-27 21:01:00 -0600

    feat: Add cats list

---
+++ js/cats.js
@@ -0,0 +1,22 @@
+(function () {
+  Cats = {};
+
+  var CatsList = [
+    {
+      name: 'fluffy',
+      image: '/img/cat_1.jpg',
+      counter: 0
+    },
+    {
+      name: 'whiskers',
+      image: '/img/cat_2.jpg',
+      counter: 0
+    },
+    {
+      name: 'sassy',
+      image: '/img/cat_3.jpg',
+      counter: 0
+    }
+  ];
+
+})();
