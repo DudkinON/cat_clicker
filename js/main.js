91f99ed1044581e3a223ed2bbbd01f1b85db767c
Oleg Dudkin <dudkin.on@gmail.com>
2017-12-27 18:40:20 -0600

    feat: Add JS event for click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 (function () {
 
+  var elem = document.getElementById('cat-container');
+  var count = document.getElementById('counter');
+  var counter = 0;
+  elem.addEventListener('click', function() {
+    counter += 1;
+    count.innerHTML = counter;
+  }, false);
+
 })();
